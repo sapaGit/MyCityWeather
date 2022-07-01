7bfdcc49ab68cda3bcdd8574611b5a68c4bf384e
sapaGit <mosevaly@gmail.com>
2022-07-01 15:24:44 +0300

    04.1 Add icons, creating request

--- MyCityWeather/ViewController.swift
+++ MyCityWeather/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     
     
-   
+//    request = "https://api.openweathermap.org/data/2.5/weather?lat=37.33233141&lon=-122.0312186&units=metric&appid=9dc585ecc48e43d2d2420f0b3d434e7d"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
